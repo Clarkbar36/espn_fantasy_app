06c8fd99bcc657fc551d09617c2c4905ec098233
Alex <alex_clark@urmc.rochester.edu>
2026-03-31 09:02:13 -0400

    adjust axis labels

--- app/pages/1_Stat Chart.py
+++ app/pages/1_Stat Chart.py
@@ -82,7 +82,7 @@ with st.container():
         y_axis_format = None  # Default formatting
 
     chart = alt.Chart(df_filtered).mark_line(point=True).encode(
-        x=alt.X("DATE:O", title="Date"),
+        x=alt.X("DATE:T", title="Date", axis=alt.Axis(format="%m-%d", tickCount="day")),
         y=alt.Y(
             f"{selected_stat}:Q",
             title=selected_stat,
@@ -93,7 +93,7 @@ with st.container():
         tooltip=[
             alt.Tooltip("teamAbbrev:N", title="Team"),
             alt.Tooltip(f"{selected_stat}:Q", title=selected_stat),
-            alt.Tooltip("DATE:O", title="Date")
+            alt.Tooltip("DATE:T", title="Date", format="%m-%d-%Y")
         ]
 
     ).properties(
