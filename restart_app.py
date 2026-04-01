5352663573aca238a9d0f612aee769e5b6c88126
Alex <alex_clark@urmc.rochester.edu>
2026-04-01 11:09:29 -0400

    railway environment id

--- restart_app.py
+++ restart_app.py
@@ -3,14 +3,15 @@ import requests
 
 RAILWAY_TOKEN = os.getenv("RAILWAY_API_TOKEN")
 SERVICE_ID = os.getenv("STREAMLIT_SERVICE_ID")
+ENVIRONMENT_ID = os.getenv("RAILWAY_ENVIRONMENT_ID")
 
-if not RAILWAY_TOKEN or not SERVICE_ID:
-    print("Missing RAILWAY_API_TOKEN or STREAMLIT_SERVICE_ID env vars")
+if not RAILWAY_TOKEN or not SERVICE_ID or not ENVIRONMENT_ID:
+    print("Missing RAILWAY_API_TOKEN, STREAMLIT_SERVICE_ID, or RAILWAY_ENVIRONMENT_ID env vars")
     exit(1)
 
 query = """
-mutation serviceInstanceRedeploy($serviceId: String!) {
-  serviceInstanceRedeploy(serviceId: $serviceId)
+mutation serviceInstanceRedeploy($serviceId: String!, $environmentId: String!) {
+  serviceInstanceRedeploy(serviceId: $serviceId, environmentId: $environmentId)
 }
 """
 
@@ -20,7 +21,7 @@ response = requests.post(
         "Authorization": f"Bearer {RAILWAY_TOKEN}",
         "Content-Type": "application/json",
     },
-    json={"query": query, "variables": {"serviceId": SERVICE_ID}},
+    json={"query": query, "variables": {"serviceId": SERVICE_ID, "environmentId": ENVIRONMENT_ID}},
 )
 
 if response.status_code == 200:
